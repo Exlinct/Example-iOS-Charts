53c1b320ab12ee8222bb099f184c7c7989d242d1
Exlinct <pham.quang.huyb@framgia.com>
2016-05-06 17:23:46 +0700

    Do display data first chart by choice year.

--- TestiOSCharts/Source/Controller/AgeOfPatient/AgeOfPatientViewController.swift
+++ TestiOSCharts/Source/Controller/AgeOfPatient/AgeOfPatientViewController.swift
@@ -8,10 +8,56 @@
 
 import UIKit
 
-class AgeOfPatientViewController: UIViewController,ChartViewDelegate {
+public extension Int {
+    /**
+     Create a random num Int
+     :param: lower number Int
+     :param: upper number Int
+     :return: random number Int
+     By DaRkDOG
+     */
+    public static func random (lower: Int , upper: Int) -> Int {
+        return lower + Int(arc4random_uniform(UInt32(upper - lower + 1)))
+    }
+}
+
+public extension Double {
+    /**
+     Create a random num Double
+     :param: lower number Double
+     :param: upper number Double
+     :return: random number Double
+     By DaRkDOG
+     */
+    public static func random(lower: Double, upper: Double) -> Double {
+//        let r = Double(arc4random_uniform(UInt32)) / Double(UInt32.max)
+//        return (r * (upper - lower)) + lower
+        return ((upper - lower) + Double(arc4random_uniform(UInt32(lower))))
+    }
+}
+
+public extension Float {
+    /**
+     Create a random num Float
+     :param: lower number Float
+     :param: upper number Float
+     :return: random number Float
+     By DaRkDOG
+     */
+    public static func random(lower: Float, upper: Float) -> Float {
+        let r = Float(arc4random()) / Float(UInt32.max)
+        return (r * (upper - lower)) + lower
+    }
+}
+
+class AgeOfPatientViewController: UIViewController,ChartViewDelegate,UITableViewDataSource,UITableViewDelegate {
 
     @IBOutlet weak var barChartView: BarChartView!
+    @IBOutlet weak var tableView: UITableView!
     
+    var arrYears:[String] = ["2011","2012","2013","2014","2015"]
+    var arrChartData:[BarChartData] = [];
+    let identifier:String = "Cell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +65,8 @@ class AgeOfPatientViewController: UIViewController,ChartViewDelegate {
         // Do any additional setup after loading the view.
         
         chartViewSetUp()
-        barChartData()
+        makeBarData()
+        setupBarChartData()
     }
     
     //MARK: - Setup BarCharView
@@ -73,9 +120,9 @@ class AgeOfPatientViewController: UIViewController,ChartViewDelegate {
         // draw label on horizontal
         xAxis.drawLabelsEnabled = true
         
-//        // set xAxis maximum value
-//        xAxis._customAxisMax = true
-//        xAxis._axisMaximum = 350
+        // set xAxis maximum value
+        xAxis._customAxisMax = true
+        xAxis._axisMaximum = 200
         
         let leftAxis: ChartYAxis = barChartView.leftAxis
         leftAxis.labelFont = UIFont.systemFontOfSize(10)
@@ -109,20 +156,82 @@ class AgeOfPatientViewController: UIViewController,ChartViewDelegate {
     
     // MARK: - Setup Data
     
-    private func barChartData() {
+    private func setupBarChartData() {
+        let barChartData:BarChartData = arrChartData[0]
+        barChartView.animate(xAxisDuration: 3.0, yAxisDuration: 3.0)
+        barChartView.data = barChartData
+    }
+    
+    // MARK: - UITableView DataSource
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.arrYears.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell! = tableView .dequeueReusableCellWithIdentifier(identifier)! as UITableViewCell
+        cell.textLabel?.text = arrYears[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let barChartData:BarChartData = arrChartData[indexPath.row]
+        barChartView.animate(xAxisDuration: 3.0, yAxisDuration: 3.0)
+        barChartView.data = barChartData
+    }
+    
+    // MARK: - Update Chart Data
+    
+    func updateChartData(index:Int) {
+        
+    }
+    
+    // MARK: - Make Bar Data
+    
+    func makeBarData() {
+        let samepleData:[[[Double]]] = self.makeSampleData()
+        for i in 0..<samepleData.count {
+            let values:[[Double]] = samepleData[i]
+            let barChartData = self.makeChartData(values)
+            
+            arrChartData.append(barChartData)
+        }
+    }
+    
+    // MARK: - Make sample chart data
+    
+    func makeSampleData() -> [[[Double]]] {
+        var values:[[[Double]]] = []
+        
+        for _ in 0...4 {
+            var pool1:[[Double]] = []
+            
+            for _ in 0...6 {
+                let value1:Double = Double.random(5.0, upper: 13.0)
+                let value2:Double = Double.random(80.0, upper: 120.0)
+                let value3:Double = Double.random(140.0, upper: 250.0)
+                let value4:Double = Double.random(40.0, upper: 80.0)
+                
+                let pool2:[Double] = [value1,value2,value3,value4]
+                pool1.append(pool2)
+            }
+            
+            values.append(pool1)
+        }
+        
+        return values
+    }
+    
+    // MARK: - Make BarChartData
+    
+    func makeChartData(values:[[Double]]) -> BarChartData {
         var xVals = [String]()
         var yVals = [BarChartDataEntry]()
-        
-        var values:[[Double]] = [
-            [11.2,95.4,151.4,41.8],
-            [10.2,113.9,151,49.5],
-            [8.4,129.3,153.2,56.9],
-            [7.1,135.2,164.6,64],
-            [6.6,129.3,181.5,74.4],
-            [6.6,112.1,215.8,75.4],
-            [6.6,94.4,244.1,76.9]
-        ]
-        
         let iYear = 2010
         
         for i in 0...6 {
@@ -143,7 +252,7 @@ class AgeOfPatientViewController: UIViewController,ChartViewDelegate {
         ]
         dataSet.valueTextColor = UIColor.whiteColor()
         dataSet.barSpace = 0.5
-        barChartView.data = BarChartData(xVals: xVals, dataSet: dataSet)
+        return BarChartData(xVals: xVals, dataSet: dataSet)
     }
 
     override func didReceiveMemoryWarning() {
@@ -151,4 +260,5 @@ class AgeOfPatientViewController: UIViewController,ChartViewDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    
 }
